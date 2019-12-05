3a956f17beea67b1837a83f4971eb018a204f82d
Georgi Gerganov <ggerganov@gmail.com>
2019-12-05 20:49:12 +0000

    close #1 : better style

---
+++ samples.js
@@ -0,0 +1,56 @@
+var samples = {
+    "sample 0" :
+`# sample 0
+
+digraph {
+    subgraph cluster_0 {
+        a0 -> a1 -> a2 -> a3;
+        label = "process #1";
+    }
+
+    subgraph cluster_1 {
+        b0 -> b1 -> b2 -> b3;
+        label = "process #2";
+    }
+
+    start -> a0;
+    start -> b0;
+    a1 -> b3;
+    b2 -> a3;
+    a3 -> a0;
+    a3 -> end;
+    b3 -> end;
+}`,
+    "sample 1" :
+`# sample 1
+
+digraph {
+    a -> b;
+    b -> c;
+    b -> d;
+}`,
+    "sample 2" :
+`# sample 2
+
+graph {
+    rankdir = LR;
+
+    a -- b;
+    b -- c;
+    a -- c;
+    d -- c;
+    e -- c;
+    e -- a;
+}`,
+    "sample 3" :
+`# sample 3
+
+digraph {
+    a -> b [ label = "0.2" ];
+    a -> c [ label = "0.4" ];
+    c -> b [ label = "0.6" ];
+    c -> e [ label = "0.6" ];
+    e -> e [ label = "0.1" ];
+    e -> b [ label = "0.7" ];
+}`,
+};
